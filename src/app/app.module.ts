069cdfe340e8eabc550a3255dee560b7df1d1ede
Maksim Vigura <mvigura@gmail.com>
2018-11-24 19:38:28 +0500

    material installation todo class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
