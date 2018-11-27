c827c74ce694d9c3a6ce1fec41677347da9ef584
Maksim Vigura <mvigura@gmail.com>
2018-11-27 14:53:52 +0500

    edit mock data

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -6,46 +6,45 @@ import * as moment from 'moment';
   providedIn: 'root'
 })
 export class TodoDataService {
-  // Placeholder for last id so we can simulate
-  // automatic incrementing of ids
-  lastId: number = 0;
-
   // Placeholder for todos
   todos: Todo[] = [
     new Todo({
-      id: 1000,
-      title: 'To do',
-      expireDate: moment(new Date().getTime() + 3 * 60 * 1000).format(
-        moment.HTML5_FMT.DATETIME_LOCAL
-      )
-    }),
-    new Todo({
-      id: 1001,
-      title: 'To do',
+      id: 1,
+      title: 'Something amazing',
       completed: true,
-      expireDate: moment(new Date().getTime() + 2 * 30 * 1000).format(
+      expireDate: moment(new Date().getTime() + 1 * 60 * 60 * 1000).format(
         moment.HTML5_FMT.DATETIME_LOCAL
       )
     }),
     new Todo({
-      id: 1002,
-      title: 'todo, todo, todo',
-      expireDate: moment(new Date().getTime() + 30 * 60 * 1000).format(
+      id: 2,
+      title: 'Lunch',
+      expireDate: moment(new Date().getTime() + 3 * 60 * 60 * 1000).format(
         moment.HTML5_FMT.DATETIME_LOCAL
       )
     }),
     new Todo({
-      id: 1003,
-      title: 'to doooo, dodododooodoooooo',
-      expireDate: moment(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).format(
+      id: 3,
+      title: 'Something beautiful',
+      expireDate: moment(new Date().getTime() + 4 * 24 * 60 * 60 * 1000).format(
+        moment.HTML5_FMT.DATETIME_LOCAL
+      )
+    }),
+    new Todo({
+      id: 4,
+      title: 'Write a book',
+      expireDate: moment(new Date().getTime() + 5 * 24 * 60 * 60 * 1000).format(
         moment.HTML5_FMT.DATETIME_LOCAL
       )
     })
   ];
 
+  // Placeholder for last id so we can simulate
+  // automatic incrementing of ids
+  lastId: number = this.todos.length + 1;
+
   constructor() {}
 
-  // Simulate POST /todos
   addTodo(todo: Todo): TodoDataService {
     if (!todo.id) {
       todo.id = ++this.lastId;
